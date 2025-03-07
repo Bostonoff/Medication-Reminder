df75c91e93113255f81aaf3542233ec5565c2527
maga <muhammadboston1101@gmail.com>
2025-03-07 10:08:47 +0100

    something realy went wrong

---
+++ MedDone/MedDone.swift
@@ -0,0 +1,17 @@
+//
+//  MediMindApp.swift
+//  MediMind
+//
+//  Created by Mukhammad Bustonov on 01/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MedDone: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
